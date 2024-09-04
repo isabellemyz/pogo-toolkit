7e0713f7f145648dc49b01c1cfa77789e40d997f
isabellemyz <isabellemyz33@gmail.com>
2024-09-04 09:14:18 -0400

    stop tracking

--- app/services/constants.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-POKEMON_DF = pd.read_csv('/Users/isabellez/pogo-toolkit/app/data/all.csv')
-TYPES_DF = pd.read_csv('https://raw.githubusercontent.com/zonination/pokemon-chart/master/chart.csv')
-MOVESET_DF = pd.read_csv('/Users/isabellez/pogo-toolkit/app/data/movesets.csv')
-ELITE_DF = pd.read_csv('/Users/isabellez/pogo-toolkit/app/data/elite.csv')
-
